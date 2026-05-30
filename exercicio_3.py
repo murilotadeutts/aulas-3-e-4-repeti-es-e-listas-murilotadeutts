791cccf061cba6d44719c7016f27d431ebf03903
murilotadeutts <murilotadeutts@gmail.com>
2026-05-30 10:05:46 -0300

    Update exercicio_3.py
    Added functionality to filter and display odd numbers from the input list.

--- exercicio_3.py
+++ exercicio_3.py
@@ -23,3 +23,11 @@ Os números ímpares são [1, 3, 5]
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+
+impares = [] # Cria uma nova lista vazia para armazenar os números ímpares
+
+for numero in lista: # Utiliza um loop for para percorrer cada número na lista original
+    if numero % 2 != 0: # Verifica se o número é ímpar utilizando o operador de módulo (%), que retorna o resto da divisão do número por 2. Se o resultado for diferente de 0, significa que o número é ímpar
+        impares.append(numero) # Se o número for ímpar, adiciona ele à lista 'impares' utilizando o método append()
+
+print(f"Os números ímpares são {impares}.")
