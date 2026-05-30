2ac46aacf6a2173ab7aaf13d6c232731ba9bf309
murilotadeutts <murilotadeutts@gmail.com>
2026-05-30 10:05:03 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -25,3 +25,11 @@ Pode-se utilizar o comando "while" ou até o "for" para te ajudar nisso.
 
 Fonte: Curso em vídeo.
 """
+
+numero = int(input("Digite um número: ")) 
+fatorial = 1 # Variável para armazenar o resultado do fatorial, iniciada com 1, pois o fatorial de 0 é 1 e o fatorial de 1 também é 1
+
+for i in range(2, numero + 1): # Utiliza um loop for para percorrer os números de 2 até o número digitado pelo usuário (inclusive), pois o fatorial de 0 e 1 é 1, então começamos a multiplicar a partir de 2
+    fatorial = fatorial * i # Multiplica cada número ao valor da variável fatorial
+
+print(f"O fatorial de {numero} é {fatorial}.")
